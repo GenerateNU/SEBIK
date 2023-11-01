b7bb8ab6572eddaa01934a00da62dbaa30328291
benyoon2003 <102326657+benyoon2003@users.noreply.github.com>
2023-11-01 09:34:37 -0400

    Add files via upload

---
+++ src/Errors.hpp
@@ -0,0 +1,30 @@
+#ifndef ERRORS_HPP
+#define ERRORS_HPP
+
+#include "Ui.hpp"
+
+enum Errors
+{
+    OVERPRESSURE,
+    OVERHEAT,
+    NONE
+};
+
+class ErrorHandler
+{
+    private:
+
+    public:
+        ErrorHandler();
+
+        ~ErrorHandler();
+
+        //TODO: handleOverpressure
+        void HandleOverpressure();
+
+        //TODO: handleOverheat
+        void HandleOverheat();
+};
+
+
+#endif
